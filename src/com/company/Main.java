30422c7649060e111d35aa43ec0dd85b943c0591
ericagleaton <EricaGleaton@comcast.net>
2019-09-13 14:58:29 -0400

    added price symbol

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -195,7 +195,7 @@ public class Main {
             orderTotal = (3+(countMeat  + countBeans + countCheese + countSalsa + countVeggies + countXguac + countXqueso + countXSourcream)* price);
  //System.out.println("orderTotal ="+ orderTotal);
             String orderStrTotal = Double.toString(orderTotal);
-            order.add(orderStrTotal);
+            order.add("Price $ : " + orderStrTotal);
 
             System.out.println("Burrito #  " + b + " " + order);
         }
